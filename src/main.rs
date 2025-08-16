a65b7947566edd723503e6106e3881ed5885c527
Aditya <adityaprasad175@gmail.com>
2025-08-16 22:49:13 +0530

    final commit before render

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
+// use rocket::fs::{FileServer, relative};
+// use std::env;
+
+// #[rocket::launch]
+// fn rocket() -> _ {
+//     let port: u16 = env::var("PORT")
+//         .unwrap_or_else(|_| "8000".into()) // fallback for local testing
+//         .parse()
+//         .expect("PORT must be a number");
+
+//     rocket::build()
+//         .mount("/", FileServer::from(relative!("static")))
+//         .configure(rocket::Config {
+//             port,
+//             address: "0.0.0.0".parse().unwrap(),
+//             ..Default::default()
+//         })
+// }
+
 use rocket::fs::{FileServer, relative};
-use std::env;
 
 #[rocket::launch]
 fn rocket() -> _ {
-    let port: u16 = env::var("PORT")
-        .unwrap_or_else(|_| "8000".into()) // fallback for local testing
-        .parse()
-        .expect("PORT must be a number");
-
     rocket::build()
         .mount("/", FileServer::from(relative!("static")))
-        .configure(rocket::Config {
-            port,
-            address: "0.0.0.0".parse().unwrap(),
-            ..Default::default()
-        })
 }
